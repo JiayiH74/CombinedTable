497c916f18da889ae30d8ae268866e07c939ae3b
JiayiH74 <jiayih12@nycstudents.net>
2025-05-06 09:52:53 -0400

    Create CombinedTable.java

---
+++ CombinedTable.java
@@ -0,0 +1,19 @@
+public class CombinedTable
+  {
+    private SingleTable one;
+    private SingleTable two;
+    public CombinedTable(SingleTable o, SingleTable t)
+    {
+      one = o;
+      two = t;
+    }
+
+    public boolean canSeat(int num)
+    {
+    }
+
+    public double getDesirability()
+    {
+    }
+  }
+
