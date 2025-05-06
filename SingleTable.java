924932315f6b5435669ca7075f060d887cf1883e
JiayiH74 <jiayih12@nycstudents.net>
2025-05-06 09:56:06 -0400

    Create SingleTable.java

---
+++ SingleTable.java
@@ -0,0 +1,28 @@
+public class SingleTable
+  {
+    private int seats;
+    private double viewQuality;
+    private int height;
+    public SingleTable(int s, double v, int h)
+    {
+      seats = s;
+      viewQuality = v;
+      height = h;
+    }
+    public int getNumSeats()
+    {
+    }
+
+    public int getHeight()
+    {
+    }
+
+    public double getViewQuality()
+    {
+      
+    }
+
+    public void setViewQuality(double value)
+    {
+    }
+  }
